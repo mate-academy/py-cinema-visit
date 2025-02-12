0da18958bce4a40823b70140be39cef693cf1fa8
Taras Shevchyk <tarashevchik27@gmail.com>
2025-02-12 11:57:08 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,31 @@
-# write your imports here
+# filepath: /d:/Games/python/mate_acad/py-cinema-visit/app/main.py
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+    customers: list,
+    number: int,
+    cleaner: str,
+    movie: str
+) -> None:
+    for customer_data in customers:
+        customers=[
+            Customer(
+                name=c["name"],
+                food=c["food"]
+            ) 
+            for c in customers
+        ]
+
+        CinemaBar.sell_product(product=customer.food, customer=customer)
+
+    hall = CinemaHall(number)
+    hall.movie_session(
+        movie_name=movie,
+        customers=[Customer(name=c["name"],
+        food=c["food"]) for c in customers],
+        cleaning_staff=Cleaner(name=cleaner)
+    )
