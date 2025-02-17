9895a42d94a34a37a0bb944ea94f9ccf60a238d5
Dmytro Deviatlytskyi <videv1410@gmail.com>
2025-02-17 16:21:38 +0100

    Cinema visit

---
+++ app/cinema/hall.py
@@ -0,0 +1,17 @@
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+
+
+class CinemaHall:
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(self,
+                      movie_title: str,
+                      customers: list[Customer],
+                      cleaning_staff: Cleaner) -> None:
+        print(f"\"{movie_title}\" started in hall number {self.number}.")
+        for customer in customers:
+            customer.watch_movie(movie_title)
+        print(f"\"{movie_title}\" ended.")
+        cleaning_staff.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    customers_list = []
+    for element in customers:
+        customer = Customer(name=element["name"], food=element["food"])
+        customers_list.append(customer)
+        CinemaBar.sell_product(product=element["food"], customer=customer)
+    cleaners_name = Cleaner(name=cleaner)
+    cinema_hall = CinemaHall(hall_number)
+    cinema_hall.movie_session(
+        movie_title=movie,
+        customers=customers_list,
+        cleaning_staff=cleaners_name
+    )
