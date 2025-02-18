9ff3c22b0d2947c48f29081a4ab6f587d6c0724b
Marek Drządzewski <dzadzumaster@gmail.com>
2025-02-18 13:05:12 +0100

    Implemented CinemaHall class

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -8,7 +8,17 @@ class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customers:List[Customer] ) -> None:
+    def movie_session(self, movie_name: str, customers:List[Customer], cleaning_staff:Cleaner) -> None:
+
+        print(f'"{movie_name}" stated in hall {movie_name}')
+        for customer in customers:
+            customer.watch_movie(f'"{movie_name}"')
+        print(f'"{movie_name}" ended')
+        cleaning_staff.clean_hall(self.number)
+
+
+
+
 
 
 
