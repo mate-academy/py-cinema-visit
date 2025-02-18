59a869a85803de38d5650f518d95913422bb43b6
Marek Drządzewski <dzadzumaster@gmail.com>
2025-02-18 15:21:54 +0100

    corrected errors from flake8

--- app/main.py
+++ app/main.py
@@ -4,7 +4,12 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
 
     customers_list = []
 
@@ -15,14 +20,6 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) ->
     cleaner = Cleaner(cleaner)
 
     for customer in customers_list:
-        CinemaBar.sell_product(customer.food,customer)
+        CinemaBar.sell_product(customer.food, customer)
 
-    cinema_hall.movie_session(movie,customers_list,cleaner)
-
-
-
-
-
-
-
-    pass
+    cinema_hall.movie_session(movie, customers_list, cleaner)
