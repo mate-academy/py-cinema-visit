5df14272163ffe3f1e8bf39c3bee395f287ba0b5
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-02-20 17:46:44 +0200

    Fixet flake number 4

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -6,9 +6,9 @@ class CinemaHall:
                       movie_name: str,
                       customers: list,
                       cleaning_staff: any) -> None:
-        print(f"\'{movie_name}\' "
+        print(f'\"{movie_name}\" '
               f"started in hall number {self.number}.")
         for customer in customers:
             customer.watch_movie(movie_name)
-        print(f"\'{movie_name}\' ended.")
+        print(f'\"{movie_name}\" ended.')
         cleaning_staff.clean_hall(self.number)
