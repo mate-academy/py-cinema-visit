f721dafa270089d258c00c5e84c5ceab3df19aba
Roman Snitsarenko <roman.snitsarenko@gmail.com>
2025-02-21 19:18:03 +0100

    Add cinema staff and customer classes with functionality for movie sessions

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.hall import CinemaHall
+from app.bar import CinemaBar
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number:
+        int, cleaner: str,
+        movie: str
+) -> None:
+    cinema_hall_obj = CinemaHall(hall_number)
+    cleaner_obj = Cleaner(cleaner)
+    customer_obj_list = []
+
+    for customer in customers:
+        customer_obj_list.append(Customer(customer["name"], customer["food"]))
+
+    for customer in customer_obj_list:
+        CinemaBar.sell_product(customer, customer.food)
+
+    cinema_hall_obj.movie_session(movie, customer_obj_list, cleaner_obj)
