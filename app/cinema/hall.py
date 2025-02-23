8be69b1c62a15c8799c3a5ea89738c36db6fee4f
Oleksa <frodo.work@gmail.com>
2025-02-23 18:57:32 +0200

    bar + hall(raw)

---
+++ app/cinema/hall.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class CinemaHall:
+
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(
+            self,
+            movie_name: str,
+            customers: List["Customer"],
+            cleaning_staff: "Cleaner"
+    ) -> None:
+        print(f"\"{movie_name}\" started in hall number {self.number}")
+
+        # TODO: print "{customer.name} is watching {film_name}."
+        # TODO: call for each customer instance: customer.watch_movie
+
+        print(f"{movie_name} ended.")
+
+        # TODO: print "Cleaner {cleaning_staff.name} is cleaning hall number {self.number}"
+        # TODO: call cleaning_staff.clean_hall()
