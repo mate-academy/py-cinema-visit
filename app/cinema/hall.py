1cca93e2ce6fea66d1ed4a2e4945d8ed10f5d485
Volodymyr Vitsenko <your@mail.com>
2025-02-23 12:40:45 +0100

    'cinema-visit'

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,19 +1,20 @@
 from __future__ import annotations
+from app.people.cinema_staff import Cleaner
 
 
 class CinemaHall:
 
-    def __init__(self, number: int):
+    def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self,
+    @staticmethod
+    def movie_session(
             customers: list,
             hall_number: int,
             cleaning_staff: Cleaner,
-            movie_name: str):
+            movie_name: str) -> None:
         print(f"{movie_name} starts in hall {hall_number}!")
         for customer in customers:
             customer.watch_movie(movie_name)
         print(f"{movie_name} ended in hall {hall_number}!")
         cleaning_staff.clean_hall(hall_number)
-
