d8f059bbdbae0efb2d31f5388a94c2b8d0ef25b2
oleanto <olegantonenko100@gmail.com>
2025-02-25 16:51:04 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -4,14 +4,19 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers_list: list, hall_number_visit: int, cleaner: str, movie_arg: str) -> None:
+def cinema_visit(customers_list: list,
+                 hall_number_visit: int,
+                 cleaner: str,
+                 movie_arg: str
+                 ) -> None:
     customer_instance = [Customer(name=cust["name"],
                                   food=cust["food"])
                          for cust in customers_list]
     cleaner_instance = Cleaner(name=cleaner)
 
     for customer in customer_instance:
-        CinemaBar.sell_product(product=customer.food, customer=customer)
+        CinemaBar.sell_product(product=customer.food,
+                               customer=customer)
 
     hall = CinemaHall(hall_number_visit)
     hall.movie_session(movie_name=movie_arg, customers=customer_instance, cleaning_staff=cleaner_instance)
@@ -25,4 +30,7 @@ def cinema_visit(customers_list: list, hall_number_visit: int, cleaner: str, mov
 # cleaner_name = "Anna"
 # movie = "Madagascar"
 #
-# cinema_visit(customers_list=customers, hall_number_visit=hall_number, cleaner=cleaner_name, movie_arg=movie)
+# cinema_visit(customers_list=customers,
+# hall_number_visit=hall_number,
+# cleaner=cleaner_name,
+# movie_arg=movie)
