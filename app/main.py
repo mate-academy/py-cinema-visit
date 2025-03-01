9b92499fd25060ab9b65531dd591554340087def
Anastasiia Matsebon <anastasiiamatsebon@gmail.com>
2025-03-01 21:18:51 +0100

    task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
-
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list, number: int, cleaner: str, movie: str) -> None:
+    customer_objects = [
+        Customer(name=c["name"], food=c["food"])
+        for c in customers
+        if "name" in c and "food" in c
+    ]
+    if not customer_objects:
+        print("No valid customers found!")
+        return
+    for cust in customer_objects:
+        CinemaBar.sell_product(customer=cust)
+    hall = CinemaHall(number)
+    cleaner_object = Cleaner(cleaner)
+    hall.movie_session(movie_name=movie, customers=customer_objects, cleaning_staff=cleaner_object)
