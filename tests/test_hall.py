0f8789cad66f8decd93e51af9af2fc6d65802a81
Roksolana Kalinska <woops359@gmail.com>
2025-03-03 10:16:29 +0200

    'Solution'

--- tests/test_hall.py
+++ tests/test_hall.py
@@ -2,9 +2,9 @@ import io
 
 from contextlib import redirect_stdout
 
-from app import CinemaHall
-from app import Cleaner
-from app import Customer
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 def test_cinema_hall_constructor():
