44c96add7aaffcbe9c88e8c09b7de916e81376c8
Artem Riabchynskyi <ryabchinskyam@gmail.com>
2025-03-06 22:31:20 +0200

    Solution

--- app/people/customer.py
+++ app/people/customer.py
@@ -4,4 +4,4 @@ class Customer:
         self.food = food  # Виправлено: foof -> food
 
     def watch_movie(self, movie: str) -> None:
-        print(f"{self.name} is watching \"{movie}\".")
+        print(f'{self.name} is watching \"{movie}\".')
