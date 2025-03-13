76a36948e3ff2ef8a3e9554e216137c173164c26
DAVYD <davfares@gmail.com>
2025-03-12 20:07:05 -0400

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ from app.cinema.hall import CinemaHall
 
 def cinema_visit(customers, hall_number, cleaner, movie):
     # Create instances of Customer, CinemaHall, and Cleaner
-    customer_instances = [Customer(name=customer["name"], food=customer["food"]) for customer in customers]
+    customer_instances = [Customer(name=customer["name"],
+                                   food=customer["food"]) for customer in customers]
     cleaner_instance = Cleaner(name=cleaner)
     hall_instance = CinemaHall(hall_number=hall_number)
 
@@ -18,4 +19,5 @@ def cinema_visit(customers, hall_number, cleaner, movie):
     hall_instance.movie_session(movie_name=movie,
                                 customers=customer_instances,
                                 cleaning_staff=cleaner_instance)
+
 
