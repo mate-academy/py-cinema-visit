f2638bc1d3903635e6aef10f7d254bb5c6e27352
DAVYD <davfares@gmail.com>
2025-03-12 20:04:00 -0400

    Solution

--- app/main.py
+++ app/main.py
@@ -2,16 +2,30 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
+from typing import List, Dict
 
 
-def cinema_visit(customers, hall_number, cleaner, movie):
+def cinema_visit(
+        customers: List[Dict[str, str]],
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
     # Create instances of Customer, CinemaHall, and Cleaner
-    customer_instances = [Customer(name=customer["name"], food=customer["food"]) for customer in customers]
+    customer_instances = [
+        Customer(name=customer["name"], food=customer["food"])
+        for customer in customers
+    ]
     cleaner_instance = Cleaner(name=cleaner)
     hall_instance = CinemaHall(hall_number=hall_number)
 
+    # Sell products to customers using keyword arguments
     for customer in customer_instances:
         CinemaBar.sell_product(product=customer.food, customer=customer)
 
     # Schedule the movie session
-    hall_instance.movie_session(movie_name=movie, customers=customer_instances, cleaning_staff=cleaner_instance)
+    hall_instance.movie_session(
+        movie_name=movie,
+        customers=customer_instances,
+        cleaning_staff=cleaner_instance
+    )
