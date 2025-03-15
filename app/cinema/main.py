a7039d76e1139f5c31fc8b19459302c9f98bd1a0
Oleh Nychyporenko <nich.oleg@ukr.net>
2025-03-15 17:02:48 +0200

    Implement py-cinema-visit

--- app/cinema/main.py
+++ app/cinema/main.py
@@ -1,7 +1,7 @@
-from app.people.customer import Customer
-from app.people.cinema_staff import Cleaner
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
 def cinema_visit(
@@ -10,14 +10,15 @@ def cinema_visit(
         cleaner: str,
         movie: str
 ) -> None:
-    cinema_bar = CinemaBar()
-    cinema_hall = CinemaHall(hall_number)
-    cleaning_staff = Cleaner(cleaner)
-    clients = []
-
+    customers_list = []
     for customer in customers:
-        client = Customer(customer["name"], customer["food"])
-        clients.append(client)
-        cinema_bar.sell_product(client, customer["food"])
+        customers_list.append(Customer(customer["name"], customer["food"]))
 
-    cinema_hall.movie_session(movie, clients, cleaning_staff)
+    hall = CinemaHall(hall_number)
+    cinema_bar = CinemaBar()
+    staff = Cleaner(cleaner)
+
+    for person in customers_list:
+        cinema_bar.sell_product(person, person.food)
+
+    hall.movie_session(movie, customers_list, staff)
