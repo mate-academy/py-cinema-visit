728518ec719c65304a15151c28df5f1c79a0c81f
Kacper Lebocinski <kaclebocinski@gmail.com>
2025-03-18 12:27:07 +0100

    Solution6

--- app/main.py
+++ app/main.py
@@ -8,7 +8,9 @@ def cinema_visit(customers: list,
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
+
     customer_instances = []
+
     for customer_data in customers:
         customer = Customer(
             customer_data["name"],
