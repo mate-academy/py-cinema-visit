ae077e708286d197e64f597ba7a8d421ef12d2dc
Kacper Lebocinski <kaclebocinski@gmail.com>
2025-03-18 12:19:43 +0100

    Solution3

--- app/main.py
+++ app/main.py
@@ -5,10 +5,7 @@ from app.people.customer import Customer
 
 
 def cinema_visit(
-        customers: list,
-        hall_number: int,
-        cleaner: str,
-        movie: str) -> None:
+        customers: list, hall_number: int, cleaner: str, movie: str) -> None:
 
     customer_instances = []
 
