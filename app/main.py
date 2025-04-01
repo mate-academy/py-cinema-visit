4e984ce963d6efb83838121b3af60433dad389b5
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-01 17:20:40 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list[dict], hall_number: int, cleaner: str, movie: str):
+    cinema_hall = CinemaHall(hall_number)
+    cleaner_instance = Cleaner(cleaner)
+    customer_instances = []
+    for customer_data in customers:
+        customer = Customer(customer_data['name'], customer_data['food'])
+        customer_instances.append(customer)
+        CinemaBar.sell_product(customer, customer.food)
+
+    CinemaHall.movie_session(cinema_hall, movie, customer_instances, cleaner_instance)
