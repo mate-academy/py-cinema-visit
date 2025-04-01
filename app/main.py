6c3a703e374657e5ca65026c824069233861006d
Vitalik <lg.mobile0808@gmail.com>
2025-04-01 13:39:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ from cinema.bar import CinemaBar
 from cinema.hall import CinemaHall
 from people.cinema_staff import Cleaner
 
+
 def cinema_visit(
     customers: list,
     hall_number: int,
     cleaner: str,
     movie: str
-):
+) -> None:
     for customer in customers:
         print(
             CinemaBar.sell_product(
