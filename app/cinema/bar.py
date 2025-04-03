88939df14be117e41a29ef77c08400b8242c8e79
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-03 15:28:33 +0300

    bar.py

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,8 @@
+from app.people.customer import Customer
+
+
 class CinemaBar:
+
     @staticmethod
-    def sell_product(product, customer):
-        print(f"Cinema bar sold {product} to {customer.name}.") 
+    def sell_product(customer: Customer, product: str) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
