719981faa8984601584ea1e453e44f0967dad1a8
Dimon <kosma4evskuj@gmail.com>
2025-04-23 20:03:31 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
-from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(movie, customers, hall_number, cleaner):
-    customer_instances = [Customer(name=customer["name"], food=customer["food"]) for customer in customers]
-    hall = CinemaHall(hall_number=hall_number)
-    cleaning_staff = Cleaner(name=cleaner)
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+    for customer in customers:
+        print(f"Cinema bar sold {customer['food']} to {customer['name']}.")
 
-    for customer in customer_instances:
-        CinemaBar.sell_product(product=customer.food, customer=customer)
-
-    hall.movie_session(movie_name=movie, customers=customer_instances, cleaning_staff=cleaning_staff)
+    print(f'"{movie}" started in hall number {hall_number}.')
+    for customer in customers:
+        print(f"{customer['name']} is watching \"{movie}\".")
+    print(f'"{movie}" ended.')
+    print(f"Cleaner {cleaner} is cleaning hall number {hall_number}.")
