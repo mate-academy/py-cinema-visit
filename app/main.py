8e7a3bb1e4cbabc3a5b9ec1c36eaa0c0ae59589f
Dimon <kosma4evskuj@gmail.com>
2025-04-23 20:32:53 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -4,12 +4,12 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
     for customer in customers:
         print(f"Cinema bar sold {customer['food']} to {customer['name']}.")
 
-    print(f'"{movie}" started in hall number {hall_number}.')
+    print(f"\"{movie}\" started in hall number {hall_number}.")
     for customer in customers:
         print(f"{customer['name']} is watching \"{movie}\".")
-    print(f'"{movie}" ended.')
+    print(f"\"{movie}\" ended.")
     print(f"Cleaner {cleaner} is cleaning hall number {hall_number}.")
