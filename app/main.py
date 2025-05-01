795fd32d716c84145ba7c2f876b9d22c4f07ebfb
greysnell <greysnell20@gmail.com>
2025-05-01 20:23:53 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from app.people import Customer, Cleaner
 from app.cinema import CinemaBar, CinemaHall
 
-def cinema_visit(movie: str, customers: list, hall_number: int, cleaner: str) -> None:
+
+def cinema_visit(
+    movie: str, customers: list, hall_number: int, cleaner: str
+) -> None:
     cinema_hall = CinemaHall(hall_number)
     cleaning_staff = Cleaner(cleaner)
     customers = [Customer(name=c["name"], food=c["food"]) for c in customers]
