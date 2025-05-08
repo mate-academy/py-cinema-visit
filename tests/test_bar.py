45eb01b267c99e5b58420194a88dd3936f205069
Oleksandr Dziuba <tornadodzu122@gmail.com>
2025-05-09 02:23:16 +0300

    Solution_09_05_25(Dziuba)

--- tests/test_bar.py
+++ tests/test_bar.py
@@ -1,4 +1,3 @@
-import pytest
 import io
 
 from contextlib import redirect_stdout
@@ -15,7 +14,7 @@ def test_cinema_bar_sell_product():
     f = io.StringIO()
 
     with redirect_stdout(f):
-        cb.sell_product(customer=customer, product=customer.food)
+        CinemaBar.sell_product(user=customer.name, product=customer.food)
 
     out = f.getvalue()
     output = "Cinema bar sold Sprite to Alice.\n"
