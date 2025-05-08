dd4361b2f687e9c90bbd456cca9d10f99703b0c2
Oleksandr Dziuba <tornadodzu122@gmail.com>
2025-05-09 02:39:01 +0300

    Solution_09_05_25(Dziuba)_V3

--- app/main.py
+++ app/main.py
@@ -9,10 +9,9 @@ def cinema_visit(
 ) -> None:
     total_list = [Customer(cut["name"], cut["food"]) for cut in customers]
     for client in customers:
-        bar_cin = CinemaBar()
-        bar_cin.sell_product(user=client["name"],
-                             product=client["food"]
-                             )
+        CinemaBar.sell_product(user=client["name"],
+                               product=client["food"]
+                               )
     human = CinemaHall(hall_number)
     clean_person = Cleaner(cleaner)
     human.movie_session(movie_name=movie, customers=total_list,
