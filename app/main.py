2fe924fcc3779b5cd85617bfd17f3141453f0487
thomasdouglasam <thomasdouglasam@gmail.com>
2025-05-08 17:10:46 -0300

    Solution #2

--- app/main.py
+++ app/main.py
@@ -4,10 +4,21 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list,
+def cinema_visit(customers: list[dict],
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
+    """
+    Simulates a cinema visit: customers buy food,
+    watch a movie, and the hall is cleaned.
+
+    :param customers: List of customer data,
+    where each item is a dict with 'name' and 'food' keys.
+    :param hall_number: The number of the cinema hall.
+    :param cleaner: The name of the cleaner responsible
+    for cleaning after the session.
+    :param movie: The title of the movie being shown.
+    """
     customers_object = [Customer(name=c["name"],
                                  food=c["food"]) for c in customers]
     cleaner_current = Cleaner(name=cleaner)
