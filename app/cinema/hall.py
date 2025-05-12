8c0f50c75a142fccbaa0a7544c24acc003c95e3b
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-12 17:00:57 +0200

    Cinema Service

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -8,11 +8,12 @@ class CinemaHall:
         self.number: int = number
 
     def movie_session(
-        self, movie_name: str, customers: List[Customer], cleaning_staff: Cleaner
+        self, movie_name: str, customers:
+            List[Customer], cleaning_staff: Cleaner
     ) -> None:
-        print(f'"{movie_name}" ' f"started in hall number {self.number}.")
+        print(f'"{movie_name}" '
+              f"started in hall number {self.number}.")
         for customer in customers:
             customer.watch_movie(movie=movie_name)
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(hall_number=self.number)
-
