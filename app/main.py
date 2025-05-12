051615df571e87da9dc838676bcd5525addfd77c
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-12 17:27:58 +0200

    Cinema Service

--- app/main.py
+++ app/main.py
@@ -6,22 +6,25 @@ from app.people.cinema_staff import Cleaner
 
 def cinema_visit(customers: dict, hall_number: int,
                  cleaner: str, movie: str) -> None:
-    customers_dict = []
+    customers_list = []
     for customer in customers:
-        customers_dict.append(Customer(name=customer["name"],
-                                       food=customer["food"]
-                                       )
-                              )
+        customers_list.append(
+            Customer(
+                name=customer["name"],
+                food=customer["food"]
+            )
+        )
 
-    for customers_bar in customers_dict:
-        (CinemaBar.sell_product
-         (product=customers_bar.food,
-                               customer=customers_bar)
-         )
+    for customer in customers_list:
+        CinemaBar.sell_product(
+            product=customer.food,
+            customer=customer
+        )
 
     hall = CinemaHall(number=hall_number)
     cleaner_instance = Cleaner(name=cleaner)
     hall.movie_session(
-        movie_name=movie, customers=customers_dict,
+        movie_name=movie,
+        customers=customers_list,
         cleaning_staff=cleaner_instance
     )
--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -4,5 +4,7 @@ class Cleaner:
 
     def clean_hall(self, hall_number: int) -> None:
         self.hall_number = hall_number
-        print(f"Cleaner {self.name} is cleaning" f" hall number {self.hall_number}.")
-
+        print(
+            f"Cleaner {self.name} is cleaning"
+            f" hall number {self.hall_number}."
+        )
