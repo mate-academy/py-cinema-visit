9087ca22c700020bc244becc7d53f0ac8dce453e
pronikito38@gmail.com <pronikito38@gmail.com>
2025-05-13 12:59:35 +0300

    my result2)

--- app/main.py
+++ app/main.py
@@ -4,7 +4,9 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list,
+from typing import List
+
+def cinema_visit(customers: List[Customer],
                  hall_number: int,
                  cleaner: str,
                  movie: str) -> None:
