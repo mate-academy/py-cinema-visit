2fdff3672917ed2feae47cd406e452eb3973b19e
Alex Shap <aleks.shaparenko@gmail.com>
2025-05-14 13:36:52 +0200

    Solution2

--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -1,8 +1,4 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from app.cinema.hall import CinemaHall
 
 
 class Cleaner:
@@ -10,5 +6,5 @@ class Cleaner:
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def clean_hall(self, hall_number: "CinemaHall.number") -> None:
+    def clean_hall(self, hall_number: int) -> None:
         print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
