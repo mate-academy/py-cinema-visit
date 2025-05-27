39c4ba38286126fc9e884a09c70387506ddaff09
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-05-27 21:15:23 +0200

    try_one

---
+++ app/cinema/hall.py
@@ -0,0 +1,12 @@
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+
+
+
+class CinemaHall:
+    def __init__(self, number) -> None:
+        self.number = number
+
+    def movie_session(self, movie_name: str, customers: "Customer", cleaning_staff: "Cleaner") -> None:
+        Customer.watch_movie(movie_name)
+        Cleaner.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    customers = Customer(customers)
+    cleaner = Cleaner(cleaner)
+    bar = CinemaBar()
+    hall = CinemaHall(hall_number)
+    bar.sell_product(customers, movie)
+    hall.movie_session(movie, customers, cleaner)
