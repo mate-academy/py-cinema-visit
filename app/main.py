d066bc7f5e11b29e9375a5f51e963675d0b6f708
Tim <tymur.prokhorov@asquad.io>
2025-05-27 15:30:40 +0300

    WIP

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
-from app.people.customer import Customer
-from app.people.cinema_staff import Cleaner
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    customer_objects = []
-    for customer_data in customers:
-        customer = Customer(name=customer_data["name"], food=customer_data["food"])
-        CinemaBar.sell_product(product=customer.food, customer=customer)
-        customer_objects.append(customer)
 
-    cinema_hall = CinemaHall(number=hall_number)
-    cleaner_obj = Cleaner(name=cleaner)
-    cinema_hall.movie_session(movie_name=movie, customers=customer_objects, cleaning_staff=cleaner_obj)
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
+    customers_list = []
+    for customer in customers:
+        customers_list.append(Customer(customer.get("name"),
+                                       customer.get("food")))
+    for customer in customers_list:
+        CinemaBar.sell_product(customer.food, customer)
+    CinemaHall(hall_number).movie_session(movie, customers_list,
+                                          Cleaner(cleaner))
