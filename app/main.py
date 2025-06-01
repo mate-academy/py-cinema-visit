2b4375d8534914a461692ccb38015d120184e3b9
Artem Shlychkin <artemshlychkin1995@gmail.com>
2025-06-01 15:31:07 +0300

    fix flake8 bugs

--- app/main.py
+++ app/main.py
@@ -4,22 +4,28 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
     # Create customer instances
-    customer_objs = [Customer(name = c ['name'], food = c ['food']) for c in customers]
+    customer_objs = [
+        Customer(name=c["name"],
+                 food=c["food"]
+                 ) for c in customers
+    ]
 
     # Sell products to customers
     for customer in customer_objs:
-        CinemaBar.sell_product(product = customer.food, customer = customer)
+        CinemaBar.sell_product(product=customer.food, customer=customer)
 
     # Create cinema hall and cleaner
-    hall = CinemaHall(number = hall_number)
-    cleaner_obj = Cleaner(name = cleaner)
+    hall = CinemaHall(number=hall_number)
+    cleaner_obj = Cleaner(name=cleaner)
 
     # Run movie session
     hall.movie_session(
-        movie_name = movie,
-        customers = customer_objs,
-        cleaning_staff = cleaner_obj
+        movie_name=movie, customers=customer_objs, cleaning_staff=cleaner_obj
     )
-
