78110ce6d4afaf2ab94a2eba7e858e15e7360e68
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-06-02 18:59:37 +0700

    All packages pass all tests and have no flake8 notices

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    for i, person in enumerate(customers):
+        pers = Customer(name=person["name"], food=person["food"])
+        CinemaBar.sell_product(customer=pers, product=pers.food)
+        customers[i] = pers
+
+    hall = CinemaHall(hall_number)
+    hall.movie_session(movie, customers, Cleaner(cleaner))
