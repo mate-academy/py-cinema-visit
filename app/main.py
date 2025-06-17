2f4383207aabfb810c76efccbe0259536df3234e
Hryhorii Seniv <hryhorii.seniv@javatar.pro>
2025-06-18 00:19:01 +0300

    Implemented task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+    created_customers = create_customer(customers)
+    hall = CinemaHall(hall_number)
+    cleaning_staff = Cleaner(cleaner)
+
+    for customer in created_customers:
+        CinemaBar.sell_product(customer.food, customer)
+
+    hall.movie_session(movie, created_customers, cleaning_staff)
+
+
+def create_customer(customers: list) -> list[Customer]:
+    return [Customer(c["name"], c["food"]) for c in customers]
