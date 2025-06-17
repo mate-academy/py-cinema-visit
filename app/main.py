416a0bcc59da4e9656e02da3a1a3b5346aa57b0c
Samuel Yudi <toyotaa038@gmail.com>
2025-06-17 16:20:00 -0300

    Primeiro commit do projeto py-cinema-visit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
-# write your imports here
+from typing import List, Dict
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
+def cinema_visit(
+    movie: str,
+    customers: List[Dict[str, str]],
+    hall_number: int,
+    cleaner: str
+):
+    customer_objs = [Customer(name=c["name"], food=c["food"]) for c in customers]
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    for cust in customer_objs:
+        CinemaBar.sell_product(product=cust.food, customer=cust)
+
+    hall = CinemaHall(hall_number=hall_number)
+    cleaning_staff = Cleaner(name=cleaner)
+
+    hall.movie_session(movie_name=movie, customers=customer_objs, cleaning_staff=cleaning_staff)
