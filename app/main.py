c37a8c89ffbadcd5c7139e4ffb832d4f5d2efee2
Samuel Yudi <toyotaa038@gmail.com>
2025-06-23 18:53:47 -0300

    dennnnerrr

--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
-from app.people.cinema_staff import Cleaner
+from app.people.cleaner import Cleaner
 
-def cinema_visit(customers, hall_number, cleaner, movie):
+def cinema_visit(customers: list, hall_number: int, cleaner_name: str, movie: str):
     customer_objs = [Customer(name=c["name"], food=c["food"]) for c in customers]
 
     for c in customer_objs:
         CinemaBar.sell_product(product=c.food, customer=c)
 
     hall = CinemaHall(hall_number=hall_number)
-    cleaning_staff = Cleaner(name=cleaner)
+    cleaning_staff = Cleaner(name=cleaner_name)
 
     hall.movie_session(movie_name=movie, customers=customer_objs, cleaning_staff=cleaning_staff)
 
--- app/people/cleaner.py
+++ app/people/cleaner.py
@@ -1,6 +1,7 @@
+# app/people/cleaner.py
 class Cleaner:
     def __init__(self, name: str) -> None:
         self.name = name
 
     def clean_hall(self, hall_number: int) -> None:
-        print(f'Cleaner {self.name} is cleaning hall number {hall_number}.')
+        print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
