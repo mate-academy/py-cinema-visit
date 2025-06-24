e603ebcfdf4e145fa0f753c1a6f3e26753c2ab21
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-06-24 17:32:03 +0300

    Created two packages(cinema and people) and all files for task py-cinema-visit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+
+    cinema_visitors = []
+    for customer in customers:
+        new_customer = Customer(customer["name"], customer["food"])
+        CinemaBar.sell_product(new_customer.food, new_customer)
+        cinema_visitors.append(new_customer)
+
+    current_session = CinemaHall(hall_number)
+    current_cleaner = Cleaner(cleaner)
+    current_session.movie_session(movie, cinema_visitors, current_cleaner)
