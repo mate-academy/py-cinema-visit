c71bec976de8946f2445e19abd4cfa021e467af5
Lukian <lukian55d@gmail.com>
2025-07-13 00:44:17 +0100

    Trigger CI checks

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+
+__all__ = ["CinemaBar", "CinemaHall"]
--- app/people/customer.py
+++ app/people/customer.py
@@ -1,10 +1,7 @@
-from __future__ import annotations
-
-
 class Customer:
     def __init__(self, name: str, food: str) -> None:
-            self.name = name
-            self.food = food
+        self.name = name
+        self.food = food
 
-    def watch_movie(self, movie: str) -> Customer:
-        print(f"{self.name} is watching a '{movie}'.")
+    def watch_movie(self, movie_name: str) -> None:
+        print(f"{self.name} is watching {movie_name}.")
