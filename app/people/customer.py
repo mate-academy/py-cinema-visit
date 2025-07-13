f5bfcad1e084d9c47ad2d123e20e2c403157a057
slop <slopcloset@gmail.com>
2025-07-13 00:08:24 -0400

    Solution

---
+++ app/people/customer.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+
+class Customer:
+    customers = []
+
+    def __init__(self, name: str, food: str) -> None:
+        self.name = name
+        self.food = food
+        Customer.customers.append(self)
+
+    def watch_movie(self, movie: str) -> None:
+        print(f"{self.name} is watching \"{movie}\".")
