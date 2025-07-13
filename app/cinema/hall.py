f0aa6db5440f0e1ecbea41f9ce93c2cd5fa24077
mr_eugene <roberttimethone@gmail.com>
2025-07-13 15:48:06 +0200

    Solution#1

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,10 +1,16 @@
-class CinemaHall:
-    def __init__(self, hall_number: int):
-        self.hall_number = hall_number
 
-    def movie_session(self, movie_name, customers, cleaning_staff):
-        print(f'"{movie_name}" started in hall number {self.hall_number}.')
+from app.people.cinema_staff import Cleaner
+
+
+class CinemaHall:
+
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(self, movie_name: str, customers: list,
+                      cleaning_staff: Cleaner) -> None:
+        print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
-        cleaning_staff.clean_hall(self.hall_number)
+        cleaning_staff.clean_hall(self.number)
--- app/main.py
+++ app/main.py
@@ -3,13 +3,22 @@ from app.people.cinema_staff import Cleaner
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+
+def cinema_visit(customers: list, number: int, cleaner: str,
+    # write you code here
+                 movie: str) -> None:
+    pass
     customer_objects = []
     for customer_data in customers:
-        customer_instance = Customer(name=customer_data["name"], food=customer_data["food"])
-        customer_objects.append(customer_instance)
-        CinemaBar.sell_product(customer=customer_instance, product=customer_data["food"])
+        name = customer_data["name"]
+        food = customer_data["food"]
+        customer = Customer(name=name, food=food)
+        CinemaBar.sell_product(product=food, customer=customer)
+        customer_objects.append(customer)
 
-    cleaner_instance = Cleaner(name=cleaner)
-    hall = CinemaHall(hall_number=hall_number)
-    hall.movie_session(movie_name=movie, customers=customer_objects, cleaning_staff=cleaner_instance)
+    hall = CinemaHall(number=number)
+    cleaning_staff = Cleaner(name=cleaner)
+    hall.movie_session(
+        movie_name=movie, customers=customer_objects,
+        cleaning_staff=cleaning_staff
+    )
