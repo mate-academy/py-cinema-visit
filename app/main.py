26103be5308e808bf2b518380fdbe0a06848033e
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-07-19 22:34:00 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    #write you code here
+    # write you code here
     pass
 def cinema_visit(
     customers: list,
