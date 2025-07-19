aef8420121d0df181641890e1da73d18d13157ad
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-07-19 22:47:12 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,9 +5,6 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
 def cinema_visit(
     customers: list,
     hall_number: int,
