d0d24461c1b1d4b0dff2416fee33ad62c11839e0
Zabodashka <igorsulga6@gmail.com>
2025-07-19 19:12:43 +0300

    Update bar.py

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -3,4 +3,5 @@ from app.people.customer import Customer
 
 class CinemaBar:
     def sell_product(self, customer: Customer, product: str) -> None:
-        print(f'Cinema bar sold {product} to {customer.name}.')
+        print(f"Cinema bar sold {product} to {customer.name}.")
+
