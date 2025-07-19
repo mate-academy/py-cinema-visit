aefdd13d70ad15c111a07ddf2c49f49d1d96e197
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-07-19 22:31:39 +0200

    Solution

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -5,8 +5,6 @@ from app.people.cinema_staff import Cleaner
 class CinemaHall:
     def __init__(self, hall_number: int) -> None:
         self.hall_number = hall_number
-        # Видалено надлишковий self.number = hall_number, оскільки ви просили використовувати hall_number
-        # Якщо інші частини вашого коду все ще покладаються на 'self.number', вам потрібно буде вирішити, який атрибут використовувати послідовно.
 
     def movie_session(
         self,
