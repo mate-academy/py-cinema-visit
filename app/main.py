4c779152eb6cd7818a38ead5d1d5bc3982e9fbfd
Oleksandr Skoromnov <solalex005@gmail.com>
2025-07-24 22:47:47 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 from app.cinema.hall import CinemaHall
-from app.cinema.bar import CinemaBar
 
 
 def cinema_visit(
