835a051ca19b180fbd88006d027608d83375bc48
Yevhen Dmytrenko <zdmitrenko26@gmail.com>
2025-08-12 14:11:30 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(
+        movie: str,
         customers: list,
         hall_number: int,
-        cleaner: str,
-        movie: str
+        cleaner: str
 ) -> None:
     customers_list = [
         Customer(customer["name"], customer["food"]) for customer in customers
