6c041d95cf9e603746d0fd1c7108113d9ec69f68
Oleksandr Polishchuk <crazy8diamond@gmail.com>
2025-08-14 13:44:06 +0300

    Implement-cinema-visit

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
+from typing import List, Dict
 
 
 def cinema_visit(
-        customers: list,
+        customers: List[Dict[str, str]],
         hall_number: int,
         cleaner: str,
         movie: str
