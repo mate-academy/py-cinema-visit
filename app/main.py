159c0b1454278e7a86ae261a6910e7b46e7aa5c6
Maciej Majewski <maciek12majewski@gmail.com>
2025-08-17 19:47:48 +0200

    test2

--- app/main.py
+++ app/main.py
@@ -2,17 +2,18 @@ from app.cinema.hall import CinemaHall
 from app.cinema.bar import CinemaBar
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
-def cinema_visit(customers: list,
-                 hall_number: int,
-                 cleaner_name: str,
-                 movie: str) -> None:
-    cinema_hall = CinemaHall(hall_number)
-    cleaning_staff = Cleaner(cleaner_name)
-    customer_list = [Customer(customer["name"], customer["food"])
-                     for customer in customers]
-    customer_list.reverse()
-    for customer in customer_list:
-        CinemaBar.sell_product(customer, customer.food)
 
+def cinema_visit(movie, customers, hall_number, cleaner):
+    # Create hall and cleaner objects
+    cinema_hall = CinemaHall(hall_number)
+    cleaning_staff = Cleaner(cleaner)
+
+    # Create Customer objects
+    customer_list = [Customer(customer["name"], customer["food"]) for customer in customers]
+
+    # Sell products to customers
+    for customer in customer_list:
+        CinemaBar.sell_product(customer.food, customer)
+
+    # Start the movie session
     cinema_hall.movie_session(movie, customer_list, cleaning_staff)
-    pass
