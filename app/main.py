c06e0efc184d9b0c625ac55971082f29ea5696a0
Maciej Majewski <maciek12majewski@gmail.com>
2025-08-17 19:13:45 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+
+from typing import List
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: List[Customer], hall_number: int, cleaner: Cleaner, movie: str) -> None:
+    ...
+
+    """
+    Function to manage serving a movie in a cinema hall.
+
+    Args:
+        customers (List[Customer]): List of customers attending.
+        hall_number (int): The hall number where the movie is being shown.
+        cleaner (Cleaner): Cleaner assigned to the hall.
+        movie (str): The movie to be played.
+    """
+    hall = CinemaHall(hall_number, cleaner, movie)
+    hall.clean_hall()
+
+    for customer in customers:
+        hall.admit_customer(customer)
+        CinemaBar.serve_food(customer)
+
