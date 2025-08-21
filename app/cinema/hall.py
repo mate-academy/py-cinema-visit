297935834c31fafc2e92c9d39aea31f0ecf1a147
Dawid <kucharzjaw@gmail.com>
2025-08-21 11:36:47 +0200

    Solution

---
+++ app/cinema/hall.py
@@ -0,0 +1,28 @@
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from typing import List
+
+
+class CinemaHall:
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(
+        self,
+        movie_name: str,
+        customers: List[Customer],
+        cleaning_staff: List[Cleaner]
+    ) -> None:
+
+        print(f"\"{movie_name}\" started in hall number {self.number}.")
+
+        if not isinstance(cleaning_staff, list):
+            cleaning_staff = [cleaning_staff]
+
+        for customer in customers:
+            print(f"{customer.name} is watching \"{movie_name}\".")
+
+        print(f"\"{movie_name}\" ended.")
+
+        for worker in cleaning_staff:
+            worker.clean_hall(self.number)
