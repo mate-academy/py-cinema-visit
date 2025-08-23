34325bb9da51118840676fc20bce0ebd61940919
Marcelo Almeida <marceloalmeidacrm@gmail.com>
2025-08-22 21:04:06 -0300

    Solution-corretion

--- app/main.py
+++ app/main.py
@@ -5,14 +5,14 @@ from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(
-    customers: list, hall_number: int, cleaner: str, movie: str
+    movie: str, customers: list, hall_number: int, cleaner: str
 ) -> None:
     customer_instances = [
         Customer(name=c["name"], food=c["food"]) for c in customers
     ]
 
     cleaner_instance = Cleaner(name=cleaner)
-    hall_instance = CinemaHall(number=hall_number)
+    hall_instance = CinemaHall(hall_number=hall_number)
 
     for customer in customer_instances:
         CinemaBar.sell_product(product=customer.food, customer=customer)
