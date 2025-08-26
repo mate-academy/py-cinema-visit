069aa99e81f39ea117b1c5be36d43dca1982b5cd
nazarkravchenko <nazarnazarik5@gmail.com>
2025-08-26 23:05:44 +0300

    check

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,11 +1,12 @@
+from typing import List
+
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
-from typing import List
 
 
 class CinemaHall:
-    def __init__(self, number: int) -> None:
-        self.number = number
+    def __init__(self, hall_number: int) -> None:
+        self.hall_number = hall_number
 
     def movie_session(
         self,
@@ -13,8 +14,8 @@ class CinemaHall:
         customers: List[Customer],
         cleaning_staff: Cleaner,
     ) -> None:
-        print(f'"{movie_name}" started in hall number {self.number}.')
+        print(f'"{movie_name}" started in hall number {self.hall_number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
-        cleaning_staff.clean_hall(self.number)
+        cleaning_staff.clean_hall(self.hall_number)
--- app/main.py
+++ app/main.py
@@ -1,32 +1,26 @@
+from typing import Dict, List
+
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
-from typing import List, Dict
 
 
 def cinema_visit(
+    movie: str,
     customers: List[Dict[str, str]],
     hall_number: int,
     cleaner: str,
-    movie: str,
 ) -> None:
-    # создаём объекты
     customer_objs: List[Customer] = [
         Customer(name=c["name"], food=c["food"]) for c in customers
     ]
-    cinema_bar = CinemaBar()
     hall = CinemaHall(hall_number)
     cleaner_obj = Cleaner(cleaner)
 
-    # бар продаёт еду
-    for customer in customer_objs:
-        cinema_bar.sell_product(
-            product=customer.food,
-            customer=customer,
-        )
+    for cust in customer_objs:
+        CinemaBar.sell_product(product=cust.food, customer=cust)
 
-    # киносеанс
     hall.movie_session(
         movie_name=movie,
         customers=customer_objs,
