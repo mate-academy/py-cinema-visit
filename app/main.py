254c245fd3b6cfce019f600a0301b6e3406ce2c0
nazarkravchenko <nazarnazarik5@gmail.com>
2025-08-26 23:01:26 +0300

    check

--- app/main.py
+++ app/main.py
@@ -2,26 +2,33 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
+from typing import List, Dict
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(
+    customers: List[Dict[str, str]],
+    hall_number: int,
+    cleaner: str,
+    movie: str,
+) -> None:
     # создаём объекты
-    customer_objs = [Customer(name=c["name"], food=c["food"]) for c in customers]
-    bar = CinemaBar()
+    customer_objs: List[Customer] = [
+        Customer(name=c["name"], food=c["food"]) for c in customers
+    ]
+    cinema_bar = CinemaBar()
     hall = CinemaHall(hall_number)
     cleaner_obj = Cleaner(cleaner)
 
     # бар продаёт еду
     for customer in customer_objs:
-        bar.sell_product(product=customer.food, customer=customer)
+        cinema_bar.sell_product(
+            product=customer.food,
+            customer=customer,
+        )
 
     # киносеанс
-    hall.movie_session(movie_name=movie, customers=customer_objs, cleaning_staff=cleaner_obj)
-
-
-if __name__ == "__main__":
-    customers = [
-        {"name": "Bob", "food": "Coca-cola"},
-        {"name": "Alex", "food": "popcorn"}
-    ]
-    cinema_visit(customers=customers, hall_number=5, cleaner="Anna", movie="Madagascar")
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_objs,
+        cleaning_staff=cleaner_obj,
+    )
