7f0e4b8abb3041f46548891bd95420175bc92b1b
Anna S <my@example.com>
2025-08-27 20:55:38 -0300

    flake8 works

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
     customer_objects = []
     for customer in customers:
         customer_obj = Customer(name=customer["name"], food=customer["food"])
