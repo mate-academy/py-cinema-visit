8d95a54c15921a36824d3f54add2f8680cdce7a9
Radoslaw Ptak <radek1026@interi.pl>
2025-08-27 09:11:52 +0200

    Change in py-cinema-visit

--- app/main.py
+++ app/main.py
@@ -4,15 +4,26 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(movie: str, customers: list, hall_number: int,
-                 cleaner: str) -> None:
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
     customers = [Customer(d["name"], d["food"]) for d in customers]
     cinema_hall = CinemaHall(hall_number)
     cleaner_instance = Cleaner(cleaner)
     for customer in customers:
         CinemaBar.sell_product(customer.food, customer)
     cinema_hall.movie_session(
-        movie_name=movie,
         customers=customers,
-        cleaning_staff=cleaner_instance
+        cleaning_staff=cleaner_instance,
+        movie_name=movie,
     )
+
+
+customers = [
+    {"name": "Bob", "food": "Coca-cola"},
+    {"name": "Alex", "food": "popcorn"}
+]
+hall_number = 5
+cleaner = "Anna"
+movie = "Madagascar"
+cinema_visit(movie=movie, customers=customers,
+             hall_number=hall_number, cleaner=cleaner)
