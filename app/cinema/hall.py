9abea9cf5e9af568269113f9d8e232be06cd7d41
0lex1y <kotov.oleksiy@icloud.com>
2025-09-02 01:29:00 +0300

    check out

---
+++ app/cinema/hall.py
@@ -0,0 +1,24 @@
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+
+class CinemaHall:
+
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def movie_session(self, movie_name: str, customers: list[Customer], cleaning_staff: Cleaner):
+        print(f'"{movie_name}" started in hall number {self.number}.')
+        for customer in customers:
+            customer.watch_movie(movie_name)
+        print(f'"{movie_name}" ended.')
+        cleaning_staff.clean_hall(self.number)
+
+if __name__ == "__main__":
+    hall = CinemaHall(5)
+    movie_name = "Madagascar"
+    customers = [
+        Customer(name="Bob", food="Coca-cola"),
+        Customer(name="Alex", food="popcorn")
+    ]
+    cleaning_staff = Cleaner(name="Anna")
+    hall.movie_session(movie_name=movie_name, customers=customers, cleaning_staff=cleaning_staff)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.hall import CinemaHall
+from app.cinema.bar import CinemaBar
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
+    customer_objects = [Customer(c["name"], c["food"]) for c in customers]
+    for customer in customer_objects:
+        CinemaBar.sell_product(customer.food, customer.name)
+    hall = CinemaHall(hall_number)
+    cleaner = Cleaner(cleaner)
+    hall.movie_session(movie, customer_objects, cleaner)
