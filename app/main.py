a6dde9b852d370c4f9f244fae6b6de4924aa16e7
0lex1y <kotov.oleksiy@icloud.com>
2025-09-02 02:13:38 +0300

    Fix all tests

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def cinema_visit(customers: list[Customer], hall_number: int,
                  cleaner: str, movie: str) -> None:
     customer_objects = [Customer(c["name"], c["food"]) for c in customers]
     for customer in customer_objects:
-        CinemaBar.sell_product(product=customer.food, customer=customer.name)
+        CinemaBar.sell_product(product=customer.food, customer=customer)
     hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaner)
     hall.movie_session(movie_name=movie,
