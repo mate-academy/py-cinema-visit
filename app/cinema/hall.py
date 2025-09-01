da3c97546f34bb1f562a1a308cc67bbe77c7ac87
0lex1y <kotov.oleksiy@icloud.com>
2025-09-02 01:35:41 +0300

    Fix flake8 errors

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,24 +1,16 @@
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
+
 class CinemaHall:
 
     def __init__(self, number: int) -> None:
         self.number = number
 
-    def movie_session(self, movie_name: str, customers: list[Customer], cleaning_staff: Cleaner):
+    def movie_session(self, movie_name: str,
+                      customers: list[Customer], cleaning_staff: Cleaner) -> str:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
-
-if __name__ == "__main__":
-    hall = CinemaHall(5)
-    movie_name = "Madagascar"
-    customers = [
-        Customer(name="Bob", food="Coca-cola"),
-        Customer(name="Alex", food="popcorn")
-    ]
-    cleaning_staff = Cleaner(name="Anna")
-    hall.movie_session(movie_name=movie_name, customers=customers, cleaning_staff=cleaning_staff)
