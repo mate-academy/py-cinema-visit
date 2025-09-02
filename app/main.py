dd0add9598c4c793cdc32c900f02eddcec811dc2
0lex1y <kotov.oleksiy@icloud.com>
2025-09-02 03:05:00 +0300

    enother one

--- app/main.py
+++ app/main.py
@@ -6,10 +6,12 @@ from app.cinema.bar import CinemaBar
 
 def cinema_visit(movie, customers, hall_number, cleaner):
     customer_list = []
-    for c in customers:
-        customer_obj = Customer(c["name"], c["food"])
-        CinemaBar.sell_product(product=customer_obj.food, customer=customer_obj)
+    for customer in customers:
+        customer_obj = Customer(customer["name"], customer["food"])
+        CinemaBar.sell_product(product=customer_obj.food,
+                               customer=customer_obj)
         customer_list.append(customer_obj)
     hall = CinemaHall(hall_number)
     cleaner_obj = Cleaner(cleaner)
-    hall.movie_session(movie_name=movie, customers=customer_list, cleaning_staff=cleaner_obj)
+    hall.movie_session(movie_name=movie,
+                       customers=customer_list, cleaning_staff=cleaner_obj)
