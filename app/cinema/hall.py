fea78efabd9230f02065f5d0ff08be121fbdce3c
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-09-03 20:24:45 +0200

    solution #5

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -4,7 +4,7 @@ from app.people.customer import Customer
 
 class CinemaHall:
     def __init__(self, hall_number: int) -> None:
-        self.hall_number  = hall_number
+        self.hall_number = hall_number
 
     def movie_session(self,
                       movie_name: str,
