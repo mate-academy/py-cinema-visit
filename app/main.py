f9a4d4ae49c9e82990866f5ca01458085cb5f5e1
Mykhaylo Zhyhan <mz5157e@gre.ac.uk>
2025-09-04 22:12:43 +0100

    all tasks realized

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
+from app.people.customer import Customer
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
-from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
@@ -9,17 +9,11 @@ def cinema_visit(customers: list[dict],
                  cleaner: str,
                  movie: str) -> None:
 
-    # Create Customer objects from the list of dictionaries
-    customer_objects = [Customer(c["name"],
-                                 c["food"]) for c in customers]
+    customer_objects = [Customer(c["name"], c["food"]) for c in customers]
 
-    # Create CinemaHall and Cleaner objects
-    hall = CinemaHall(hall_number)
-    cleaning_staff = Cleaner(cleaner)
-
-    # Sell food to customers at the cinema bar
     for cust in customer_objects:
         CinemaBar.sell_product(cust.food, cust)
 
-    # Schedule the movie session and clean the hall after the movie
+    hall = CinemaHall(hall_number)
+    cleaning_staff = Cleaner(cleaner)
     hall.movie_session(movie, customer_objects, cleaning_staff)
