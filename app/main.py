b4e18bf980943122323ecd51ec35cee0c7e6ccfa
Vladislav <grechkosij.vlados@gmail.com>
2025-09-04 16:04:25 +0300

    implement cinema_visit and finish modules

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-# write your imports here
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+    hall_num = CinemaHall(hall_number)
+    cleaner_obj = Cleaner(cleaner)
+    customer_obj = []
+
+    for person in customers:
+        customer_obj.append(Customer(person["name"], person["food"]))
+
+    for obj in customer_obj:
+        CinemaBar.sell_product(product=obj.food, customer=obj)
+
+    hall_num.movie_session(movie, customer_obj, cleaner_obj)
