207afd9dc2d1f1d3407cd79c789b8c9c0ebf5c98
Maks Kaplunovskiy <kaplunovskymv@gmail.com>
2025-09-05 22:50:26 +0300

    implement py-cinema-visit fix_1

--- app/main.py
+++ app/main.py
@@ -8,11 +8,10 @@ def cinema_visit(customers: list, hall_number: int,
                  cleaner: str, movie: str) -> None:
     cinema_hall = CinemaHall(hall_number)
     cleaner = Cleaner(cleaner)
-    customers_obj = [Customer(customer["name"],
-                              customer["name"]) for customer in customers]
+    customers_obj = [Customer(customer["name"], customer["food"])
+                     for customer in customers]
 
-    for customer in customers:
-        CinemaBar.sell_product(Customer(customer["name"],
-                                        customer["food"]), customer["food"])
+    for customer in customers_obj:
+        CinemaBar.sell_product(customer.food, customer)
 
     cinema_hall.movie_session(movie, customers_obj, cleaner)
