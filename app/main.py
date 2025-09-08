d83e2e39a9a4c15600c1a1a3c60b0dfd0b7b5c13
Fles <aleksacat13@gmail.com>
2025-09-08 12:42:53 +0200

    still vscode web but changed flake8

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
-from app.people.cinema_staff import Cleaner 
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
-    customers_instances = [Customer(name=data["name"], food=data["food"]) for data in customers]
+def cinema_visit(customers: list,
+                hall_number: int,
+                cleaner: str,
+                movie: str) -> None:
+    customers_instances = [Customer(name=data["name"],
+                            food=data["food"])
+                            for data in customers]
     hall = CinemaHall(number=hall_number)
     staff = Cleaner(name=cleaner)
 
