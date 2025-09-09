7612dc88c710b2acee392887a410ec376541eb25
acidcor <deadfoxua1337@gmail.com>
2025-09-09 22:35:29 +0200

    hall_number issue

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -5,8 +5,8 @@ from app.people.customer import Customer
 
 
 class CinemaHall:
-    def __init__(self, hall_number: int) -> None:
-        self.hall_number = hall_number
+    def __init__(self, number: int) -> None:
+        self.hall_number = number
 
     def movie_session(
         self, movie_name: str, customers: list[Customer],
