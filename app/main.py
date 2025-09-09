54e9039523d90aaf04174159df5d14290e7b8054
acidcor <deadfoxua1337@gmail.com>
2025-09-09 22:27:56 +0200

    key-words fix

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str)\
-        -> None:
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
 
     customers_lst = [
         Customer(customer["name"], customer["food"]) for customer in customers
@@ -14,6 +13,6 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str)\
     cleaner_inst = Cleaner(cleaner)
 
     for customer in customers_lst:
-        CinemaBar.sell_product(customer.food, customer)
+        CinemaBar.sell_product(customer=customer, product=customer.food)
 
-    cinema_hall.movie_session(movie, customers_lst, cleaner_inst)
+    cinema_hall.movie_session(movie_name=movie, customers=customers_lst, cleaning_staff=cleaner_inst)
