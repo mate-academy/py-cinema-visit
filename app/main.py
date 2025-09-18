836b01e2f75efdcde6ccb8070f51dbfa833b1724
mohammad-abu-assad <m7mad114@gmail.com>
2025-09-18 23:34:29 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -11,13 +11,13 @@ def cinema_visit(
     movie: str,
 ) -> None:
     customer_objects = [
-        Customer(name=c["name"], food=c["food"]) for c in customers
+        Customer(name=c['name'], food=c['food']) for c in customers
     ]
 
     for customer in customer_objects:
         CinemaBar.sell_product(product=customer.food, customer=customer)
 
-    hall = CinemaHall(number=hall_number)
+    hall = CinemaHall(hall_number=hall_number)
     cleaning_staff = Cleaner(name=cleaner)
     hall.movie_session(
         movie_name=movie,
