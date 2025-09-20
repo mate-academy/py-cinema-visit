6e163a406a4e1302f644fae20430d5d2ea0056d4
Anton Tsikhanovich <alluspeh@gmail.com>
2025-09-20 18:13:15 +0200

    Solution after review

--- app/people/cinema_staff.py
+++ app/people/cinema_staff.py
@@ -3,7 +3,7 @@ class Cleaner:
         self.name = name
 
     def clean_hall(self, hall_number: int) -> None:
-        print(f'Cleaner {self.name} is cleaning hall number {hall_number}.')
+        print(f"Cleaner {self.name} is cleaning hall number {hall_number}.")
 
 
 if __name__ == "__main__":
