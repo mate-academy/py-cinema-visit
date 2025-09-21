4b24a9182cfe90ed6e0b47314655b165c79c32d9
tomech <109527747+tomech4@users.noreply.github.com>
2025-09-21 20:29:08 +0200

    complete hall module

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,3 +1,7 @@
+import app.people.customer as customer
+import app.people.cinema_staff as cinema_staff
+
+
 class CinemaHall:
     def __init__(self, hall_number: int) -> None:
         self.number = hall_number
@@ -5,10 +9,11 @@ class CinemaHall:
     def movie_session(
             self,
             movie_name: str,
-            customers: list[Customer],
-            cleaning_staff: Cleaner
+            customers: list[customer.Customer],
+            cleaning_staff: cinema_staff.Cleaner
     ):
         print(f"{movie_name} started in hall number {self.number}.")
-        customer.watch_move(customers)
+        for cust in customers:
+            cust.watch_movie(movie_name)
         print(f"{movie_name} ended.")
-        cinema_staff.clean_hall(cleaning_staff)
+        cleaning_staff.clean_hall(self.number)
