d5493c6129f44dee0f3a0ed66eefcd2b3c6f0bcc
tomech <109527747+tomech4@users.noreply.github.com>
2025-09-21 21:02:50 +0200

    add missing qoutes in prints of movie_session method

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -3,8 +3,8 @@ import app.people.cinema_staff as cinema_staff
 
 
 class CinemaHall:
-    def __init__(self, hall_number: int) -> None:
-        self.number = hall_number
+    def __init__(self, number: int) -> None:
+        self.number = number
 
     def movie_session(
             self,
@@ -12,8 +12,8 @@ class CinemaHall:
             customers: list[customer.Customer],
             cleaning_staff: cinema_staff.Cleaner
     ):
-        print(f"{movie_name} started in hall number {self.number}.")
+        print(f"\"{movie_name}\" started in hall number {self.number}.")
         for cust in customers:
             cust.watch_movie(movie_name)
-        print(f"{movie_name} ended.")
+        print(f"\"{movie_name}\" ended.")
         cleaning_staff.clean_hall(self.number)
