5542e83e3bdef653d66fbe97c042f71e0a30db8c
tomech <109527747+tomech4@users.noreply.github.com>
2025-09-21 21:32:43 +0200

    fix quotes in prints to avoid escaping double quotes

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,5 +1,5 @@
-import app.people.customer as customer
-import app.people.cinema_staff as cinema_staff
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
 class CinemaHall:
@@ -9,11 +9,11 @@ class CinemaHall:
     def movie_session(
             self,
             movie_name: str,
-            customers: list[customer.Customer],
-            cleaning_staff: cinema_staff.Cleaner
+            customers: list[Customer],
+            cleaning_staff: Cleaner
     ) -> None:
-        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        print(f'"{movie_name}" started in hall number {self.number}.')
         for cust in customers:
             cust.watch_movie(movie_name)
-        print(f"\"{movie_name}\" ended.")
+        print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
