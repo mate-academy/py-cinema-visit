52830abf0d02463e3dd7c8b1b9f934bcdc236b68
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-09-25 11:20:58 +0200

    flake8 rules

--- app/main.py
+++ app/main.py
@@ -4,15 +4,22 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
     customers_on_movie = []
-    cb= CinemaBar()
-    # customers_on_movie = [Customer(customer["name"], customer["food"]) for customer in customers]
-    # [CinemaBar.sell_product(customer, customer["food"]) for customer in customers]
+    cb = CinemaBar()
     for customer in customers:
         customers_on_movie.append(Customer(customer["name"], customer["food"]))
         food = customer.get("food")
         cb.sell_product(Customer(customer["name"], customer["food"]), food)
     cinema_cleaner = Cleaner(cleaner)
     hall = CinemaHall(hall_number)
-    hall.movie_session(movie_name = movie, customers= customers_on_movie, cleaning_staff=cinema_cleaner)
+    hall.movie_session(
+        movie_name=movie,
+        customers=customers_on_movie,
+        cleaning_staff=cinema_cleaner
+    )
