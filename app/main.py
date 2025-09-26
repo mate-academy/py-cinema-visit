9930e32eb8ca6ee77d3084ce8ff9e544f23dcd88
Maksym Dazidov <maksymdazidov@gmail.com>
2025-09-26 17:05:17 +0200

    first 6

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
-# from app.people.customer import Customer
+from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
@@ -8,6 +8,8 @@ def cinema_visit(movie: str, customers: list, hall_number: int, cleaner: str)\
         -> None:
     ch = CinemaHall(hall_number)
     clean = Cleaner(cleaner)
-    for i in customers:
-        CinemaBar.sell_product(i.name, i.food)
-    ch.movie_session(movie, customers, clean.name)
+    for u in customers:
+        Customer(u["name"], u["food"])
+    for customer in customers:
+        CinemaBar.sell_product(customer.name, customer.food)
+    ch.movie_session(movie_name=movie, customers=customers, cleaning_staff=clean)
