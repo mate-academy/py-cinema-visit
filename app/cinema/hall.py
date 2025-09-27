c909d62f6ca69ee16cffeda5756dd64229db0d8e
Stas Sytnykov <stassytnykov@gmail.com>
2025-09-27 16:46:32 +0300

    feat: add CinemaHall class

---
+++ app/cinema/hall.py
@@ -0,0 +1,7 @@
+class CinemaHall:
+    def __init__(self, number) -> None:
+        self.number = number
+
+    @staticmethod
+    def movie_session(movie_name: str, costumers: list, cleaning_staff: dict) -> None:
+        pass
