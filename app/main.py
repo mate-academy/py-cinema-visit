3be475fd67c33295d43f062ab0cdc028c8c3cbb5
Olena Pyslar <pislar33@gmail.com>
2025-09-28 21:58:16 +0300

    Fix flake8 mistake

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
     customer_list = [
         Customer(customer["name"], customer["food"])
         for customer in customers
@@ -12,6 +16,9 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     cleaner = Cleaner(cleaner)
 
     for customer in customer_list:
-        CinemaBar.sell_product(product=customer.food, customer=customer)
+        CinemaBar.sell_product(product=customer.food,
+                               customer=customer)
 
-    cinema_hall.movie_session(movie_name=movie, customers=customer_list, cleaning_staff=cleaner)
+    cinema_hall.movie_session(movie_name=movie,
+                              customers=customer_list,
+                              cleaning_staff=cleaner)
