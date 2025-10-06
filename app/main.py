220d47845fd43e688b43c7add543d6d4bd136e14
Victor Borden <vborden2022@fau.edu>
2025-10-05 21:01:21 -0400

    Fixed Parameter Name

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.people.cinema_staff import Cleaner
 
 def cinema_visit(
         movie: str,
-        customers: list[dict],
+        customers: list,
         hall_number: int,
         cleaner: str
 ) -> None:
@@ -18,7 +18,7 @@ def cinema_visit(
     cleaning_staff = Cleaner(name=cleaner)
     hall = CinemaHall(hall_number=hall_number)
     hall.movie_session(
-        movie=movie,
+        movie_name=movie,
         customers=customer_objects,
         cleaning_staff=cleaning_staff
     )
