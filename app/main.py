71a121d7eca8894d7510d7099e92d4d4768ab9c7
Ertyrten <101129428+Ertyrten@users.noreply.github.com>
2025-10-10 18:14:54 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(customers: list, hall_number: int, cleaner: str, 
+                 movie: str) -> None: # Line split here to fix E501
     # write you code here
     
     # 1. Створення екземпляра Cleaner
