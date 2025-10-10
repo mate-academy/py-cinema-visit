70918e0a6df0875ec6d9daf44ea5ac77ebbebeb4
Ertyrten <101129428+Ertyrten@users.noreply.github.com>
2025-10-10 18:22:37 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
-
-
 def cinema_visit(customers: list, hall_number: int, cleaner: str, 
                  movie: str) -> None:
     # write you code here
