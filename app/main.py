48cade99fdcc6aa0929cfb69cf8cd8843539f4eb
Serhii <spridoloba@gmail.com>
2025-10-12 23:22:08 +0300

    new solution with fix

--- app/main.py
+++ app/main.py
@@ -4,29 +4,16 @@ from app.cinema.hall import CinemaHall
 from app.cinema.bar import CinemaBar
 
 
-def cinema_visit(movie: str, customers: list,
-                 hall_number: int, cleaner: str) -> None:
-    if not isinstance(customers, list) and isinstance(movie, list):
-        real_customers: list = movie
-        real_hall: int = customers
-        real_cleaner: str = hall_number
-        real_movie: str = cleaner
-    else:
-        real_movie = movie
-        real_customers = customers
-        real_hall = hall_number
-        real_cleaner = cleaner
-
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
     customer_objects: list[Customer] = [
-        Customer(name=cust["name"],
-                 food=cust["food"]) for cust in real_customers
+        Customer(name=item["name"], food=item["food"]) for item in customers
     ]
 
-    for customer_obj in customer_objects:
-        CinemaBar.sell_product(product=customer_obj.food,
-                               customer=customer_obj)
+    for customer in customer_objects:
+        CinemaBar.sell_product(product=customer.food, customer=customer)
 
-    hall = CinemaHall(real_hall)
-    staff = Cleaner(real_cleaner)
+    hall = CinemaHall(hall_number)
+    staff = Cleaner(cleaner)
 
-    hall.movie_session(real_movie, customer_objects, staff)
+    hall.movie_session(movie, customer_objects, staff)
