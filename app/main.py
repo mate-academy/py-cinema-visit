9c8deeef6cbc6d22a2ba3f626ddc2b8a77e0f938
Tania Kolesnik <kolesnik@gmail.com>
2025-10-16 22:06:13 +0100

    add solution 1a

--- app/main.py
+++ app/main.py
@@ -1,18 +1,19 @@
-# write your imports here
-from tkinter.font import names
-
 from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
+
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     cleaner = Cleaner(cleaner)
     cinema_hall = CinemaHall(hall_number)
-
-    customers = [Customer(name=customer.get("name"), food=customer.get("food")) for customer in customers]
-
+    customers = [Customer(
+        name=customer.get("name"),
+        food=customer.get("food")) for customer in customers]
     for customer in customers:
         CinemaBar.sell_product(product=customer.food, customer=customer)
-    cinema_hall.movie_session(movie_name=movie, customers=customers, cleaning_staff=cleaner)
-
+    cinema_hall.movie_session(
+        movie_name=movie,
+        customers=customers,
+        cleaning_staff=cleaner
+    )
