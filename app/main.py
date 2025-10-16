a5090c3486c2fec715d25421508a3a5a3f2a3da7
Nik <nikita.pitushkin31@mail.com>
2025-10-16 16:32:54 +0300

    fix error

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
     customer_objects = []
     for customer in customers:
         cust = Customer(name=customer["name"], food=customer["food"])
@@ -13,7 +13,12 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
 
     cleaning_staff = Cleaner(name=cleaner)
     hall = CinemaHall(number=hall_number)
-    hall.movie_session(movie_name=movie, customers=customer_objects, cleaning_staff=cleaning_staff)
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_objects,
+        cleaning_staff=cleaning_staff
+    )
+
 
 customers = [
     {"name": "Bob", "food": "popcorn"},
