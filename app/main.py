f198886256bf9f2051ed151a39c99fa147b7a48e
Nik <nikita.pitushkin31@mail.com>
2025-10-17 12:57:58 +0300

    fix error5

--- app/main.py
+++ app/main.py
@@ -4,18 +4,13 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(
-        customers: list,
-        hall_number: int,
-        cleaner: str,
-        movie: str
-) -> None:
+def cinema_visit(movie, customers, hall_number, cleaner):
 
     customer_objects = []
     for customer in customers:
         cust = Customer(name=customer["name"], food=customer["food"])
         customer_objects.append(cust)
-        CinemaBar.sell_product(product=cust.food, customer=cust)
+        CinemaBar.sell_product(customer=cust, product=cust.food)
 
     cleaning_staff = Cleaner(name=cleaner)
     hall = CinemaHall(number=hall_number)
