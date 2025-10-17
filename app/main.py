83665cc0128853a1158830de4b9100a456efb105
Vorongor <mr.maddarknes@gmail.com>
2025-10-17 17:10:09 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,26 @@
 # write your imports here
+from typing import TypedDict
+from app.people.customer import Customer
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+class CustomerStats(TypedDict):
+    name: str
+    food: str
+
+
+def cinema_visit(customers: list[CustomerStats],
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
+    customers_list = [Customer(customer["name"], customer["food"])
+                      for customer in customers]
+    cleaner_staff = Cleaner(cleaner)
+    cinema_hall = CinemaHall(hall_number)
+
+    for customer in customers_list:
+        CinemaBar.sell_product(customer, customer.food)
+
+    cinema_hall.movie_session(movie, customers_list, cleaner_staff)
