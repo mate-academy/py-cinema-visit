6615708cfa478decde7665ed6a687f81e7eb7d32
Wojciech Witkowski <witus85@gmail.com>
2025-10-23 01:07:49 +0200

    Revert "Solution"
    This reverts commit 5c4dbe54aa91d57f8e183a0db47a202e43083c0d.

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -12,10 +12,10 @@ class CinemaHall:
             self,
             movie_name: str,
             customers: list[Customer],
-            cleaning_staff: Cleaner
+            cleaning_stuff: Cleaner
     ) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
             customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
-        cleaning_staff.clean_hall(self.number)
+        cleaning_stuff.clean_hall(self.number)
