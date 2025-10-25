bc9fe3cf688a8bbc6482928381f9e5b5c556fdb0
Alex <midgard044@gmail.com>
2025-10-26 00:16:03 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def cinema_visit(
         hall_number: int,
         cleaner: str,
         movie: str
-) -> str:
+) -> None:
     new_customers = [
         Customer(customer["name"], customer["food"]) for customer in customers
     ]
