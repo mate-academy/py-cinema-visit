a75523e5ae65c5155261cf39df8ae997ea7a9f07
Oleg <oleggavelya@gmail.com>
2025-10-26 11:21:26 +0200

    Reviewing mistakes

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
     customers = [Customer(customer["name"], customer["food"]) for customer in customers]
-    cinema_hall = CinemaHall(hall_number)
-    cleaners = Cleaner(cleaner)
     for customer in customers:
         CinemaBar.sell_product(customer, customer.food)
+    cinema_hall = CinemaHall(hall_number)
+    cleaners = Cleaner(cleaner)
     cinema_hall.movie_session(movie, customers, cleaners)
