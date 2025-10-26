481d3563f1cf4f5c688ad6832ed65fd5c877abe3
Oleg <oleggavelya@gmail.com>
2025-10-26 11:35:16 +0200

    change cinema_visit parameters

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
+def cinema_visit(movie: str, customers: list, hall_number: int, cleaner: str):
     customers = [Customer(customer["name"], customer["food"]) for customer in customers]
     for customer in customers:
-        CinemaBar.sell_product(customer, customer.food)
+        CinemaBar.sell_product(customer.food, customer)
     cinema_hall = CinemaHall(hall_number)
     cleaners = Cleaner(cleaner)
     cinema_hall.movie_session(movie, customers, cleaners)
