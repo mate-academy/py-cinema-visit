6a3e45b512c5599a5b2068deba50ff436ee9443d
Your Name <you@example.com>
2025-10-29 17:53:50 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-# write your imports here
+from typing import Iterable
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: Iterable[Customer], hall_number: int, cleaner: Cleaner, movie: str):
+    for customer in customers:
+        CinemaBar.sell_product(customer.name, customer.food)
+    CinemaHall(hall_number).movie_session(movie, customers, cleaner)
