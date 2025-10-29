436a653bf185b43f90dd79568d54ec179e156388
Ostap Shp <stark171705@gmail.com>
2025-10-29 09:26:58 +0200

    Fixed issues

--- app/main.py
+++ app/main.py
@@ -4,8 +4,10 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int,
-                 cleaner: str, movie: str) -> None:
+def cinema_visit(customers: list,
+                 hall_number: int,
+                 cleaner: str,
+                 movie: str) -> None:
     customers_objs = []
     for customer in customers:
         cus = Customer(name=customer["name"], food=customer["food"])
