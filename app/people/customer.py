07b4bdadac7e0c85424a595a2197689e0124958c
stadnikruslan168-hub <stadnikruslan168@gmail.com>
2025-10-29 15:32:17 +0200

    'Solution'

--- app/people/customer.py
+++ app/people/customer.py
@@ -3,4 +3,4 @@ class Customer:
         self.name = name
         self.food = food
     def watch_movie(self, movie: str):
-        print(f'{self.name} is watching "{movie}".')
+        print(f'{self.name} is watching "{movie}".\n')
