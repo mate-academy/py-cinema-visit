5175bfbfc9197c0116e83da7d25f780f53d787aa
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-10-30 19:05:43 +0100

    task ready

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,4 +1,7 @@
+from typing import Any
+
+
 class CinemaBar:
     @staticmethod
-    def sell_product(customer: str, product: str) -> None:
+    def sell_product(customer: Any, product: str) -> None:
         print(f"Cinema bar sold {product} to {customer.name}.")
