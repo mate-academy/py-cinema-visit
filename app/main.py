02e3ac7a03c1000e253f822e7fca3c48fd831b86
Andrii Chychkan <mywork1728@gmail>
2025-10-30 09:31:38 +0200

    cinema_visit works correctly

--- app/main.py
+++ app/main.py
@@ -3,11 +3,12 @@ from app.people.customer import Customer
 from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
 
+
 def cinema_visit(customers: list,
                  hall_number: int,
                  cleaner: str,
-                 movie: str):
-    current_cleaner = Cleaner(name=cleaner)
+                 movie: str) -> None:
+    current_cleaner = Cleaner(cleaner)
     current_hall = CinemaHall(hall_number)
     customers_objs = []
 
@@ -17,14 +18,3 @@ def cinema_visit(customers: list,
         CinemaBar.sell_product(product=customer.food, customer=customer,)
 
     current_hall.movie_session(movie, customers_objs, current_cleaner)
-
-
-sctmr = [
-    {"name": "Bob", "food": "Coca-cola"},
-    {"name": "Alex", "food": "popcorn"}
-]
-
-hlnmbr = 5
-clnrname = "Anna"
-mv = "Madagascar"
-cinema_visit(sctmr, hlnmbr, clnrname, mv)
