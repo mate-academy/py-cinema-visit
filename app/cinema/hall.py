0eac4ccf0d44c770606d691fc59d0320e32bc788
Oleksandr Prymak <prymak.olexandr@lll.kpi.ua>
2025-10-30 20:35:30 +0200

    Changed type hints from str to None and changed "movie_name" parameter in watch_movie to movie

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -10,9 +10,9 @@ class CinemaHall:
             self,
             movie_name: str,
             customers: list[Customer],
-            cleaning_staff: Cleaner) -> str:
+            cleaning_staff: Cleaner) -> None:
         print(f'"{movie_name}" started in hall number {self.number}.')
         for customer in customers:
-            customer.watch_movie(movie_name=movie_name)
+            customer.watch_movie(movie=movie_name)
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(hall_number=self.number)
--- app/people/customer.py
+++ app/people/customer.py
@@ -3,5 +3,5 @@ class Customer:
         self.name = name
         self.food = food
 
-    def watch_movie(self, movie_name: str) -> str:
-        print(f'{self.name} is watching "{movie_name}".')
+    def watch_movie(self, movie: str) -> None:
+        print(f'{self.name} is watching "{movie}".')
