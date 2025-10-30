441eb72f260fcc7f73bb9ed95e4f8ddfaaa7da79
Nikita <hkorobko@gmail.com>
2025-10-30 14:40:14 +0200

    Cinema visit

---
+++ app/cinema/customer.py
@@ -0,0 +1,7 @@
+class Customer:
+    def __init__(self, name: str, food: str) -> None:
+        self.name = name
+        self.food = food
+
+    def watch_movie(self) -> None:
+        print(f"{self.name} is watching the movie.")
