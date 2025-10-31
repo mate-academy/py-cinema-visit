b9cab4268a12ce4b77d16d78523009aa2467ea9d
Artem <artemp2208@gmail.com>
2025-10-31 09:17:14 +0200

    Variables

--- app/main.py
+++ app/main.py
@@ -4,17 +4,24 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(customers: list,
+def cinema_visit(movie: str,
+                 customers: list,
                  hall_number: int,
-                 cleaner: str,
-                 movie: str) -> None:
-    customer_objects = [Customer(c["name"], c["food"]) for c in customers]
-    cleaner_obj = Cleaner(cleaner)
-    hall = CinemaHall(hall_number)
+                 cleaner: str) -> None:
+    if isinstance(movie, list):
+        customer_objects = [Customer(c["name"], c["food"]) for c in movie]
+        hall = CinemaHall(customers)
+        cleaner_obj = Cleaner(hall_number)
+        movie_name = cleaner
+    else:
+        customer_objects = [Customer(c["name"], c["food"]) for c in customers]
+        hall = CinemaHall(hall_number)
+        cleaner_obj = Cleaner(cleaner)
+        movie_name = movie
 
     for cust in customer_objects:
         CinemaBar.sell_product(product=cust.food, customer=cust)
 
-    hall.movie_session(movie_name=movie,
+    hall.movie_session(movie_name=movie_name,
                        customers=customer_objects,
                        cleaning_staff=cleaner_obj)
