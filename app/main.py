daed6e5ba5c9634260026b296d3343423717770f
Artem <artemp2208@gmail.com>
2025-10-31 09:23:59 +0200

    if/else del

--- app/main.py
+++ app/main.py
@@ -4,24 +4,17 @@ from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
 
-def cinema_visit(movie: str,
-                 customers: list,
+def cinema_visit(customers: list,
                  hall_number: int,
-                 cleaner: str) -> None:
-    if isinstance(movie, list):
-        customer_objects = [Customer(c["name"], c["food"]) for c in movie]
-        hall = CinemaHall(customers)
-        cleaner_obj = Cleaner(hall_number)
-        movie_name = cleaner
-    else:
-        customer_objects = [Customer(c["name"], c["food"]) for c in customers]
-        hall = CinemaHall(hall_number)
-        cleaner_obj = Cleaner(cleaner)
-        movie_name = movie
+                 cleaner: str,
+                 movie: str) -> None:
+    customer_objects = [Customer(c["name"], c["food"]) for c in customers]
+    cleaner_obj = Cleaner(cleaner)
+    hall = CinemaHall(hall_number)
 
     for cust in customer_objects:
         CinemaBar.sell_product(product=cust.food, customer=cust)
 
-    hall.movie_session(movie_name=movie_name,
+    hall.movie_session(movie_name=movie,
                        customers=customer_objects,
                        cleaning_staff=cleaner_obj)
