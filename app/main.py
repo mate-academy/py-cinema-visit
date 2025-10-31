e49bea42430ea94913dbbd9f51eeac29030c4e8f
Taras Ivashchuk <tvishy@disroot.org>
2025-10-31 14:36:25 +0200

    Refactoring

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ def cinema_visit(
         customers: list,
         hall_number: int,
         cleaner: str,
-        movie: str) -> None:
+        movie: str
+) -> None:
     customers = list(
         map(lambda customer_record: Customer(
             customer_record["name"],
