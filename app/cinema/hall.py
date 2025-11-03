60d1e042d0d36d7224a72a3ee7f5703fe99ee0ed
Paweł Dudek <dudek136@gmail.com>
2025-11-03 21:50:46 +0100

    Solution v.9

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -14,14 +14,3 @@ class CinemaHall:
             customer.watch_movie(movie_name)
         print(f'"{movie_name}" ended.')
         cleaning_staff.clean_hall(self.number)
-
-# hall = CinemaHall(number=5)
-# movie_name = "Madagascar"
-# customers = [
-#     Customer(name="Bob", food="Coca-cola"),
-#     Customer(name="Alex", food="popcorn")
-# ]
-# cleaning_staff = Cleaner(name="Anna")
-#
-# hall.movie_session(movie_name=movie_name,
-# customers=customers, cleaning_staff=cleaning_staff)
--- app/main.py
+++ app/main.py
@@ -4,10 +4,6 @@ from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 
 
-# def cinema_visit(movie: str,
-#                  customers: list,
-#                  hall_number: int,
-#                  cleaner: str) -> None:
 def cinema_visit(customers: list,
                  hall_number: int,
                  cleaner: str,
@@ -21,20 +17,3 @@ def cinema_visit(customers: list,
     hall.movie_session(movie_name=movie,
                        customers=clients,
                        cleaning_staff=staff)
-
-# customers = [
-#     {"name": "Bob", "food": "Coca-cola"},
-#     {"name": "Alex", "food": "popcorn"}
-# ]
-# hall_number = 5
-# cleaner_name = "Anna"
-# movie = "Madagascar"
-# cinema_visit(customers=customers, hall_number=hall_number,
-# cleaner=cleaner_name, movie=movie)
-# Cinema bar sold Coca-cola to Bob.
-# Cinema bar sold popcorn to Alex.
-# "Madagascar" started in hall number 5.
-# Bob is watching "Madagascar".
-# Alex is watching "Madagascar".
-# "Madagascar" ended.
-# Cleaner Anna is cleaning hall number 5.
