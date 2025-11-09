98eabb5b50b311a794f2ef9499a12eedd812981f
Oleksandr Vilenskiy <128734566+AlexFoxUA@users.noreply.github.com>
2025-11-09 15:04:00 +0200

    Sol cinema 12

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -1,11 +1,12 @@
+from app.people.customer import Customer
+
+
 class CinemaBar:
-    def sell_product(
-        self,
-        customer: object = None,
-        product: str = None,
-        customer_name: str = None
-    ) -> None:
-        if customer is not None and hasattr(customer, "name"):
-            print(f"Cinema bar sold {product} to {customer.name}.")
+    def sell_product(self, product: str, customer) -> None:
+
+        if isinstance(customer, Customer):
+            customer_name = customer.name
         else:
-            print(f"Cinema bar sold {customer} to {product}.")
+            customer_name = customer
+
+        print(f"Cinema bar sold {product} to {customer_name}.")
