032a576b6a1f1ce9caca96f6c22a9d3774dfc364
Mykola Petrenko <mykola.petrenko@gmail.com>
2025-11-12 20:39:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) -> None:
+
     customers_obj = []
     for data in customers:
         customer_instance = Customer(name=data["name"], food=data["food"])
@@ -14,21 +15,3 @@ def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str) ->
     cleaner_instance = Cleaner(name=cleaner)
     hall.movie_session(movie, customers_obj, cleaner_instance)
 
-
-
-if __name__ == '__main__':
-    customers = [
-        {"name": "Bob", "food": "Coca-cola"},
-        {"name": "Alex", "food": "popcorn"}
-    ]
-    hall_number = 5
-    cleaner_name = "Anna"
-    movie = "Madagascar"
-    cinema_visit(customers=customers, hall_number=hall_number, cleaner=cleaner_name, movie=movie)
-    # Cinema bar sold Coca-cola to Bob.
-    # Cinema bar sold popcorn to Alex.
-    # "Madagascar" started in hall number 5.
-    # Bob is watching "Madagascar".
-    # Alex is watching "Madagascar".
-    # "Madagascar" ended.
-    # Cleaner Anna is cleaning hall number 5.
