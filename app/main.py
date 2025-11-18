b80597aa7149017b8be7de68d1474106b08f23df
Felipe Pessin <fhpessin@gmail.com>
2025-11-18 09:19:21 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,25 @@
-# write your imports here
+import datetime
+import time as t
+from random import randint
+from math import ceil, floor
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def print_current_time() -> None:
+    print(t.time())
+
+
+def generate_random_tuple(size: int) -> tuple:
+    return (randint(-10, 10) for _ in range(size))
+
+
+def floor_division(a: int, b: int) -> int:
+    return floor(a / b)
+
+
+def ceil_division(a: int, b: int) -> int:
+    return ceil(a / b)
+
+
+def create_today_date() -> datetime:
+    return datetime.date.today()
+
