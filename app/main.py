329f82b7e5ec4a3e3af7ada825595704d593e458
Felipe Pessin <fhpessin@gmail.com>
2025-11-18 09:33:04 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,14 +12,13 @@ def generate_random_tuple(size: int) -> tuple:
     return (randint(-10, 10) for _ in range(size))
 
 
-def floor_division(a: int, b: int) -> int:
-    return floor(a / b)
+def floor_division(value_a: int, value_b: int) -> int:
+    return floor(value_a / value_b)
 
 
-def ceil_division(a: int, b: int) -> int:
-    return ceil(a / b)
+def ceil_division(value_a: int, value_b: int) -> int:
+    return ceil(value_a / value_b)
 
 
 def create_today_date() -> datetime:
     return datetime.date.today()
-
