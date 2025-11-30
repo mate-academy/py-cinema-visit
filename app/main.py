69b17aefa5a72a315e4fa03764aa6f12adeefc77
Mykola Mosiienko <koljamos@gmail.com>
2025-11-30 17:35:33 +0000

    Implemented Cinema simulation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
-# write your imports here
+from app.people.cinema_staff import Cleaner
+from app.people.customer import Customer
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
+    Customer.peoples.clear()
+    for person in customers:
+        Customer(person["name"], person["food"])
+    pers = CinemaHall(hall_number)
+    staff = Cleaner(cleaner)
+    for obj in Customer.peoples:
+        CinemaBar.sell_product(obj.food, obj)
+    pers.movie_session(movie, Customer.peoples, staff)
