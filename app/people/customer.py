505833b654c1a6d19f337672ddb35749b465e938
Michał Wiśniewski <istius1@gmail.com>
2025-12-01 21:15:32 +0100

    fixing functions definition lines in py-cinema-visit

--- app/people/customer.py
+++ app/people/customer.py
@@ -1,6 +1,6 @@
 class Customer:
 
-    def __init__(self, name: str, food: list) -> None:
+    def __init__(self, name: str, food: str) -> None:
         self.name = name
         self.food = food
 
