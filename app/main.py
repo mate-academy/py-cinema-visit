f4f20bed5a98204b47ba5d913eb08a08c995eb31
RoboStorePL <robostorepl@gmail.com>
2025-12-08 23:33:54 +0100

    Refactor cinema_visit by removing comments
    Removed comments for clarity in cinema_visit function.

--- app/main.py
+++ app/main.py
@@ -5,21 +5,16 @@ from app.people.cinema_staff import Cleaner
 
 
 def cinema_visit(customers, hall_number, cleaner, movie):
-    # Create Customer instances
     customer_instances = []
     for customer_data in customers:
         customer = Customer(name=customer_data["name"], food=customer_data["food"])
         customer_instances.append(customer)
-
-    # Sell products to customers
     for customer in customer_instances:
         CinemaBar.sell_product(customer=customer, product=customer.food)
 
-    # Create CinemaHall and Cleaner instances
     hall = CinemaHall(number=hall_number)
     cleaner_instance = Cleaner(name=cleaner)
 
-    # Start movie session
     hall.movie_session(
         movie_name=movie,
         customers=customer_instances,
