daace05348bb9dbddbd1f65fe686b24a3dcddb98
Dmytro Burtsev <burtsev.dmytro91@gmail.com>
2025-12-08 21:14:43 +0100

    style: Fixed all remaining flake8 issues and completed the project

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,11 +1,12 @@
 from typing import List, TYPE_CHECKING
+
 if TYPE_CHECKING:
     from app.people.customer import Customer
     from app.people.cinema_staff import Cleaner
 
 
 class CinemaHall:
-    def __init__(self, number: int):
+    def __init__(self, number: int) -> None:
         self.number = number
 
     def movie_session(
@@ -14,8 +15,11 @@ class CinemaHall:
             customers: List["Customer"],
             cleaning_staff: "Cleaner"
     ) -> None:
-        print(f"\"{movie_name}\" started in hall number {self.number}.")
+        print(f'"{movie_name}" started in hall number {self.number}.')
+
         for customer in customers:
             customer.watch_movie(movie=movie_name)
-        print(f"\"{movie_name}\" ended.")
+
+        print(f'"{movie_name}" ended.')
+
         cleaning_staff.clean_hall(hall_number=self.number)
