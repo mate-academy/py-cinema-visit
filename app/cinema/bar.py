6a1853ae733e1950814222110b0c64e7d0df26be
Kyryl <sautakiki.com@gmail.com>
2025-12-10 20:05:10 +0200

    solution3

--- app/cinema/bar.py
+++ app/cinema/bar.py
@@ -4,4 +4,4 @@ from typing import Any
 class CinemaBar:
     @staticmethod
     def sell_product(product: str, customer: Any) -> None:
-        print(f"Cinema bar sold {product} to {customer.name}")
+        print(f"Cinema bar sold {product} to {customer.name}.")
