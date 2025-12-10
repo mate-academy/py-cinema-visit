e6396c4cf4b920b8b15b8dc3424de0c334c48372
Kyryl <sautakiki.com@gmail.com>
2025-12-10 19:19:48 +0200

    sol4

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -6,9 +6,9 @@ class CinemaHall:
         self.number = number
 
     def movie_session(self, movie_name: str,
-                customers: list, cleaning_staff: Any) -> None:
-        print(f'"{movie_name}" started in '
-              f'hall number {self.number}')
+                    customers: list, cleaning_staff: Any) -> None:
+        print(f"'{movie_name}' started in "
+              f"hall number {self.number}")
         for customer in customers: customer.watch_movie(movie=movie_name)
         print(f'"{movie_name}" ended')
         cleaning_staff.clean_hall(hall_number=self.number)
