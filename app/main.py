5dc92ecad62b543388701c04128d3184536c9161
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-12 04:27:17 -0500

    Flake 8 correction

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
-from app.cinema.bar import CinemaBar
 from app.cinema.hall import CinemaHall
 from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    customers_objects = [Customer(customer["name"], customer["food"]) 
+def cinema_visit(customers: list, hall_number: int,
+                 cleaner: str, movie: str) -> None:
+    customers_objects = [Customer(customer["name"], customer["food"])
                          for customer in customers]
-    cinema_object = CinemaHall(hall_number)
+    hall_object = CinemaHall(hall_number)
     cleaner_object = Cleaner(cleaner)
 
-    hall_object = CinemaHall(hall_number)
     hall_object.movie_session(movie, customers_objects, cleaner_object)
