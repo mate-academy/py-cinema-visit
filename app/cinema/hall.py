faf0d5d592dd4c1b40255a6b3ee6bc36a6a5d547
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-13 05:44:07 -0500

    Changed movie string to allow appostrophes

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -9,8 +9,14 @@ class CinemaHall:
                       cleaning_staff: object) -> None:
         for customer in customers:
             CinemaBar.sell_product(customer, customer.food)
-        print(f'"{movie_name}" started in hall number {self.number}.')
+
+        movie_start = movie_name + " started in hall number " + str(self.number) + "."
+        print(movie_start)
+
         for customer in customers:
             customer.watch_movie(movie_name)
-        print(f'"{movie_name}" ended.')
+
+        movie_end = movie_name + " ended."
+        print(movie_end)
+
         cleaning_staff.clean_hall(self.number)
