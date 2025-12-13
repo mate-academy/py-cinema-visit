e0311c86814b5897d6a24c38cde5f8ca90c611a5
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-13 06:42:50 -0500

    Removed trailing spaces

--- app/main.py
+++ app/main.py
@@ -3,19 +3,20 @@ from app.people.cinema_staff import Cleaner
 from app.people.customer import Customer
 from app.cinema.bar import CinemaBar
 
-def cinema_visit(customers: [dict], 
+
+def cinema_visit(customers: [dict],
                  hall_number: int,
-                 cleaner: str, 
+                 cleaner: str,
                  movie: str) -> None:
 
-    customers_objects = [Customer(customer["name"], 
+    customers_objects = [Customer(customer["name"],
                                   customer["food"])
                          for customer in customers]
-    
+
     hall_object = CinemaHall(hall_number)
     cleaner_object = Cleaner(cleaner)
 
     for customer in customers_objects:
         CinemaBar.sell_product(customer, customer.food)
-            
+
     hall_object.movie_session(movie, customers_objects, cleaner_object)
