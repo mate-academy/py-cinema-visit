8c8db84d24228a73cf50522b09db8a7a76f0292a
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-13 06:00:21 -0500

    Added guillemets for movie name

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -10,14 +10,14 @@ class CinemaHall:
         for customer in customers:
             CinemaBar.sell_product(customer, customer.food)
 
-        movie_start = movie_name + " started in hall number "
+        movie_start = "\"" + movie_name + "\" started in hall number "
         movie_start += str(self.number) + "."
         print(movie_start)
 
         for customer in customers:
             customer.watch_movie(movie_name)
 
-        movie_end = movie_name + " ended."
+        movie_end = "\"" + movie_name + "\" ended."
         print(movie_end)
 
         cleaning_staff.clean_hall(self.number)
