a98a2c4dfd409ac88a565c121c5c85456bd570b4
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-13 06:44:06 -0500

    Removed trailing spaces

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -1,6 +1,7 @@
 from app.people.customer import Customer
 from app.people.cinema_staff import Cleaner
 
+
 class CinemaHall:
     def __init__(self, number: int) -> None:
         self.number = number
